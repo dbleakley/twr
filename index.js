a059277fa9add9cf14cd09f7552c5816d73154e8
Darrell Bleakley <dbleakley@salesforce.com>
2021-11-18 17:23:34 -0500

    fix: req fix

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('Thunder Web Runtime');
 console.log('Thunder Web Runtime - feature one');
 console.log('security fix applied');
 console.log("service bug fix applied");
+console.log("req bug fix applied");
