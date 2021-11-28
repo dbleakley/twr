468a777b17ee530e1d52b9ff617ee80f2e9c7ec2
Darrell Bleakley <dbleakley@salesforce.com>
2021-11-28 17:54:33 -0500

    fix: bug fix c

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log("Thunder Web Runtime - feature three");
 console.log("Thunder Web Runtime - feature four");
 console.log("Thunder Web Runtime - bug fix a");
 console.log("Thunder Web Runtime - bug fix b");
+console.log("Thunder Web Runtime - bug fix c");
