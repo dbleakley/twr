38892a3815d85f87d06aec5fbcb6459ef8daeb1b
Darrell Bleakley <dbleakley@salesforce.com>
2021-11-28 13:58:17 -0500

    feat: add feature 4

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('Thunder Web Runtime');
 console.log('Thunder Web Runtime - feature one');
 console.log("Thunder Web Runtime - feature two");
 console.log("Thunder Web Runtime - feature three");
+console.log("Thunder Web Runtime - feature four");
